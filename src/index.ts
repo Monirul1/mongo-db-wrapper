cb89a9501b51ea5709e6dbb938be618227f9e5e2
Md Monirul Islam <mmonirul.islam@fordham.edu>
2023-02-18 20:48:45 -0500

    adding comments to index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,23 @@
+/*
+index.ts: Demonstrates an example of how a client can import mongo-wrapper
+library and use it's interfaces to interact with mongoDB in a node application setting
+*/
+
 import { MongoDbWrapper } from "./mongo-wrapper";
 
+// init the class and connect to the db
 const wrapper = new MongoDbWrapper();
 await wrapper.connect('mongodb://localhost:27017/mydb');
+
+const db = wrapper.getDb()
+
+//print db name
+console.log("DB name:", db.databaseName)
+
+// create a collection
 const collection = wrapper.getCollection<any>('users');
+
+// insert an item into the users collection
 await collection.insertOne(
   { 
     name: "Monirul",
@@ -11,14 +26,10 @@ await collection.insertOne(
     email: "im2594@columbia.edu"
   },
 );
+
+// retrieve an item from users collection
 const user = await collection.findOne({ name: 'Monirul' });
 console.log(user);
+
+// close the db connection
 await wrapper.disconnect();
-
-
-// const mongoDbWrapper = new MongoDbWrapper();
-// mongoDbWrapper.connect("mongodb://localhost:27017");
-
-// const db = mongoDbWrapper.getDb();
-
-// console.log(db)
