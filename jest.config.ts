745583160519a14ff7afa36b55bb0ba5feaf55ca
Md Monirul Islam <mmonirul.islam@fordham.edu>
2023-03-11 14:37:25 -0500

    added github actions

--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,7 @@ module.exports = {
   preset: "ts-jest",
   testEnvironment: "node",
   roots: ["<rootDir>/src"],
-  testTimeout: 10000,
+  testTimeout: 60000,
   moduleDirectories: ["node_modules", "src"],
   moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
 };
